217dd18f465adcfa573ac1bbcc306e7c1a4c5b99
minnehkamau <minnieches08@gmail.com>
2022-12-13 00:29:02 +0300

    rendering data

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 #FlatDango-Code-Challennge
 
+Movie show application
+
+
+
+This application is for movie shows ticket booking. It allows users to accessn movies and get to know show time.Users can be able to go through the list of movies in the application. They can also be able to choose the movie they are interested in because of the description.
+
+
+Capacity for each movie is listed so users can be able to know how many tickets are left. They can also be able to know when to book for the movie since when all seats are booked, there can never be more booking.
+
+
+This project applies HTML,CSS and javascript.It aimes to make ticket booking easier for movie shows.
+
--- index.js
+++ index.js
@@ -1,5 +1,63 @@
+//manipulating the DOM
+document.addEventListener("DOMContentLoaded", ( ) =>{
+    getMovies();
+})
+
+//fetch movies from Json server
 function getMovies(){
-    fetch("  http://localhost:3000/movies")
-    .then((response)=>response.json())
-    .then(moviesB)
+fetch("http://localhost:3000/movies")
+.then((response) => response.json())
+.then (function (data){
+    showMovies(data);
+});
+}
+
+// async function getMovies(){
+
+//     try{
+//         const response = await fetch("http://localhost:3000/movies")
+//         return await response.json()
+//     }catch (error){
+//         console.error(error)
+//     }
+// }
+// let movieShow ={}
+// getMovies().then (data => movieShow =data)
+
+// const showEvent=document.que
+function showMovies(data){
+    const movieList = document.querySelector('ul#list');
+
+    for (let i =0; i<data.length; i++){
+        const movie = data[i];
+        const filmLi = document.createElement('li');
+        filmLi.innerHTML = movie.title;
+
+
+        const line = document.createElement('hr');
+        movieList.appendChild(line);
+        movieList.appendChild(filmLi);
+        filmLi.addEventListener('click',function(){
+            movieDetails(movie)
+        })
+    }
+}
+
+function movieDetails(movie){
+    const movieDetails= document.getElementById('show-movie');
+    movieDetails.innerHTML=`
+    
+    <p id ="movietag">Title:${movie.title}</p>
+    <p id="brief">Description:${movie.description}</p> 
+    <img id = "size" src="${movie.poster}">
+
+    <p>Show Time:${movie.showtime}</p>
+    <p>Runtime:${movie.runtime}</p>
+    <p id="capacit">Capacity:${movie.capacity}</p>
+    <P id="tickets">Tickets Available:${movie.tickets_sold}</p>
+   
+    
+
+
+`
 }
